fcdfa62481cf5ef006bae75a6feeb1c288f7bdce
Gustavo Guzmán <gustavoguzman86@hotmail.com>
2020-04-08 18:08:06 +0100

    Update generic.sql

--- generic.sql
+++ generic.sql
@@ -2,7 +2,7 @@ WITH MOD_TABLE AS
 (
 SELECT ID, event_date, to_char(event_date, 'Day') DOW, substr(line,-24,7) Location, Level
 FROM data1.table1
-WHERE event_date >+ DATE '2020-01-01'
+WHERE event_date >= DATE '2020-01-01'
 AND Level in ('Level1', 'Level2', 'Level3')
 )
 SELECT * FROM MOD_TABLE
